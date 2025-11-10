599aa735598d5981f20535ea2cb7869c09222db0
AbdelrahmanKhalid710 <abdelrahmankhalid148@gmail.com>
2025-11-11 00:17:35 +0200

    Creating Authentication Service and Authentication Guard and Initializing the Firebase project configurations (#2)
    * AddingFirebaseProjectConfigurationInEnvironmentFile
    
    * `Updated dependencies to install firebase and modified Authentication Service file to use Firebase authentication`
    
    * `moved auth-related files to the correct services path`
    
    * `Updated auth guard to use Angular Router and AuthService and check if the user is authenticated`

--- frontend/src/app/app/core/guards/auth-guard.ts
+++ frontend/src/app/app/core/guards/auth-guard.ts
@@ -1,5 +1,23 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivate,Router, UrlTree } from '@angular/router';
+import { AuthService } from '../services/auth-service';
+import { Injectable } from '@angular/core';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class authGuard implements CanActivate  {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+ canActivate(): boolean | UrlTree {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    } else {
+      // Redirect user to login if not authenticated
+      return this.router.createUrlTree(['/auth/login']);
+    }
+  }
 };
