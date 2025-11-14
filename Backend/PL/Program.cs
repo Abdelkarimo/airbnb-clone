97fa5d8d33b8d26475c7c26c986b03bbefcad29f
AbdallahAssem <abdallahassem3@gmail.com>
2025-11-14 14:45:57 +0200

    installingofSignalR&CORSinProgram.cs

--- Backend/PL/Program.cs
+++ Backend/PL/Program.cs
@@ -1,3 +1,6 @@
+using BLL.Services.Abstractions;
+using BLL.Services.Impelementation;
+using PL.Hubs;
 
 namespace PL
 {
@@ -13,9 +16,31 @@ namespace PL
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            //Team Member: Abdallah Assem
+            // Date: 10-11-2025
+            // Descriprion: Add SignalR and Chat Service / Configure CORS for Angular
+            // Add SignalR and Chat Service
+            builder.Services.AddSignalR();
 
+            
+
+            
+            
+            
+            builder.Services.AddScoped<IChatService, ChatService>();
+
+            // Configure CORS for Angular
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AngularClient", policy =>
+                {
+                    policy.WithOrigins("http://localhost:4200", "http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                });
+            });
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -25,11 +50,19 @@ namespace PL
 
             app.UseHttpsRedirection();
 
+            // Use CORS
+            app.UseCors("AngularClient");
+
             app.UseAuthorization();
 
 
             app.MapControllers();
 
+            // Map SignalR Hub
+            app.MapHub<ChatHub>("/chatHub");
+            
+            ///////////End of Team Member: Abdallah Assem            
+        
             app.Run();
         }
     }
